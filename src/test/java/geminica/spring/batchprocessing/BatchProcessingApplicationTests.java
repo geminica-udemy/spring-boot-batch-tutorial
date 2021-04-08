afae18520134585bb096d299598e6ddd5d754240
Jacek Centkowski <geminica.programs@gmail.com>
2021-04-08 12:20:57 +0200

    Initial commit
    Note that java was formatted with gjf and pom.xml was formatted with
    export XMLLINT_INDENT="    "
          git ls-files | grep pom.xml | xargs -I'{}' xmllint --output '{}' --format '{}'

---
+++ src/test/java/geminica/spring/batchprocessing/BatchProcessingApplicationTests.java
@@ -0,0 +1,11 @@
+package geminica.spring.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
